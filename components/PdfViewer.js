91a0a0bf2c44e2ac84a0bbb9dfd5d5b267228cff
edisonskiploop <edison@skiploop.com>
2018-02-25 14:03:09 +0530

    Pdf View Controls Added

--- components/PdfViewer.js
+++ components/PdfViewer.js
@@ -10,6 +10,7 @@ import Pdf from 'react-native-pdf';
 import {connect} from 'react-redux';
 import * as actionCreators from '../actionCreators';
 import * as constants from './constants';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 
 export class PdfViewer extends Component {
     constructor(props) {
@@ -21,7 +22,7 @@ export class PdfViewer extends Component {
         this.pdf = null;
     }
 
-    prePage=()=>{
+    prevPage=()=>{
         if (this.pdf){
             let prePage = this.state.page>1?this.state.page-1:1;
             this.pdf.setNativeProps({page: prePage});
@@ -46,24 +47,43 @@ export class PdfViewer extends Component {
         console.log('PDF Url: ' + this.props.fileUrl);
 
         return (
-            <Pdf ref={(pdf) => {
-                this.pdf = pdf;
-            }}
-                 source={source}
-                 page={1}
-                 horizontal={false}
-                 onLoadComplete={(pageCount) => {
-                     this.setState({pageCount: pageCount});
-                     console.log(`total page count: ${pageCount}`);
-                 }}
-                 onPageChanged={(page, pageCount) => {
-                     this.setState({page: page});
-                     console.log(`current page: ${page}`);
-                 }}
-                 onError={(error) => {
-                     console.log(error);
-                 }}
-                 style={styles.pdf}/>
+           <View style={styles.container}>
+               <Pdf ref={(pdf) => {
+                   this.pdf = pdf;
+               }}
+                    source={source}
+                    page={1}
+                    horizontal={false}
+                    onLoadComplete={(pageCount) => {
+                        this.setState({pageCount: pageCount});
+                        console.log(`total page count: ${pageCount}`);
+                    }}
+                    onPageChanged={(page, pageCount) => {
+                        this.setState({page: page});
+                        console.log(`current page: ${page}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    style={styles.pdf}/>
+               <View style={styles.controlsBar}>
+                   <TouchableHighlight style={styles.arrowWrapper} onPress={() => {
+                       if(this.state.page > 1) {
+                           this.prevPage();
+                       }
+                   }}>
+                       <MaterialIcon name="arrow-upward" color="#fff" size={20}/>
+                   </TouchableHighlight>
+                   <Text style={styles.pageCount}>{this.state.page}/{this.state.pageCount}</Text>
+                   <TouchableHighlight style={styles.arrowWrapper} onPress={() => {
+                       if(this.state.page < this.state.pageCount) {
+                           this.nextPage();
+                       }
+                   }}>
+                       <MaterialIcon name="arrow-downward" color="#fff" size={20}/>
+                   </TouchableHighlight>
+               </View>
+           </View>
         );
     }
 }
@@ -72,25 +92,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginTop: 25,
+        alignItems: 'center'
     },
-    btn: {
-        margin: 5,
-        padding:5,
-        backgroundColor: "blue",
-    },
-    btnDisable: {
-        margin: 5,
-        padding:5,
-        backgroundColor: "gray",
-    },
-    btnText: {
-        color: "#FFF",
+    controlsBar: {
+        height: 50,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
     pdf: {
         flex:1,
         width:Dimensions.get('window').width,
+    },
+    arrowWrapper: {
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        padding: 5,
+        borderRadius: 4
+    },
+    pageCount: {
+        color: '#fff'
     }
 });
 
