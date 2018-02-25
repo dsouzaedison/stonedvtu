008a2361483ec6da0cd44348d2b68dc1261ea38c
edisonskiploop <edison@skiploop.com>
2018-02-25 21:45:50 +0530

    Results Page - Interstitial Ad Integrated

--- components/WebViewer.js
+++ components/WebViewer.js
@@ -2,8 +2,21 @@ import React, {Component} from 'react';
 import {
     WebView
 } from 'react-native';
+import {
+    AdMobBanner,
+    AdMobInterstitial,
+    PublisherBanner,
+    AdMobRewarded
+} from 'react-native-admob';
 
 export default class WebViewer extends Component {
+    componentDidMount() {
+        if(this.props.navigation.state.params.type && this.props.navigation.state.params.type === 'results') {
+            AdMobInterstitial.setAdUnitID('ca-app-pub-5210992602133618/2025314533');
+            AdMobInterstitial.requestAd().then(() => AdMobInterstitial.showAd());
+        }
+    }
+
     render() {
         return (
             <WebView
